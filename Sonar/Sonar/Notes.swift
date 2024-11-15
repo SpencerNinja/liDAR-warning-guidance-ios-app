48efe49978f032f5aa5945e1e9288812dfa6ea0c
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2024-11-15 15:19:22 -0700

    functionality working, bugs fixed, whole screen tappable

---
+++ Sonar/Sonar/Notes.swift
@@ -0,0 +1,38 @@
+//
+//  Notes.swift
+//  Sonar
+//
+//  Created by Spencer Hurd on 11/15/24.
+//
+
+/**
+ 
+ TODO:
+    
+ - ✅ implement LiDAR scanning
+ - ✅ implement sound when object too close
+ - ✅ implement vibrate when object too close
+ - ✅ design main screen
+ - ✅ make start button cover lower half of screen
+ - ✅ make stop button
+ - ✅ switch between start and stop button
+ - ✅ make a settings button for top half of screen
+ - settings:
+     - ✅ turn haptics on/off
+     - ✅ turn audio beeps on/off
+     - turn audible instructions off
+ 
+- add start message:
+    - visual
+    - audible
+    - "Welcome to Sonar. This app is designed to assist the visually impaired with safely navigating a world full of obstacles. Disclaimer: This tool is not meant to replace vision, but is a tool to use along with your other senses and devices to help you navigate."
+ 
+ - logo & appIcon
+
+- design landing screen
+- bats, smokescreen layer effect
+ 
+- link app to Apple Watch
+- add haptics to Apple Watch
+ 
+ */
