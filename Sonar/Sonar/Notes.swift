de82c9b96825bd7e522dc09aece0b9b9a25c05d3
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2024-11-20 21:52:47 -0700

    updated notes backlog

--- Sonar/Sonar/Notes.swift
+++ Sonar/Sonar/Notes.swift
@@ -34,10 +34,34 @@
  - ✅make sure there is only 1 speech triggering on each page
  
  BACKLOG
- - trigger "warning length" setup on so that the user sets their default "warning length"
+ - Does LiDAR work on windows and reflective floors like tile?
+ - trigger "warning length" setup wizard on so that the user sets their default "warning length"
+ - settings should include a setup wizard for warning distance
  - trigger small beeps for any change in distance greater than 0.3 feet
  - use different types of beeps to tell user if distance increases or decreased (this should catch scenarios where there is a drop in distance as well)
-- link app to Apple Watch
-- add haptics to Apple Watch
+ - improve haptic feedback time
+ - implement user-agreement screen
+    - email or save acceptances in a database (or we may not need to save the info)
+    - redirect deniers to catch screen (logo, copyright, button to user-agreement)
+ - link app to Apple Watch
+ - add haptics to Apple Watch
+ - implement free version and paid version
+    - free version: sonar and setup wizard only
+    - paid version: settings for beeps, haptics, warning distance increments
+ - come up with a better name and logo
+ - app screen screenshots and description
+ 
+ 
+ 
+ User path
+ 1. first-time opening the app
+    a. Launch Screen
+    b. User-agreement and disclaimer: Accept or Deny (A "Deny" send them to a Launch Screen where there is only the logo, copyright, and button to the User Agreement)
+    c. Warning Distance setup wizard
+ 2. Everytime:
+    a. Animated Logo screen
+    b. The user can listen to the whole Safety Acknowledgement or skip by tapping anywhere
+    c. Main menu
+ 
  
  */
